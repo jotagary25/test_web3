f4081a1fd2e3ebae1ca685587e65e44ead417e4c
DuvanVilladiego <duvan.villadiego.oficial@gmail.com>
2022-07-29 15:11:15 -0500

    feature: fractionate nft implemented

--- src/pages/BuyNft/BuyNft.jsx
+++ src/pages/BuyNft/BuyNft.jsx
@@ -7,11 +7,16 @@ const BuyNft = () => {
   const { account } = useWeb3React();
   const fractionalNft = useFractionalNft();
   const [status, setStatus] = useState({});
-  const [isMintin, setMintin] = useState(false);
+  const [slices, setSlices] = useState(2);
+  const [tokenId, setTokenId] = useState();
+  const [isFractionate, setIsFractionate] = useState(false);
+  const [isToken, setIsToken] = useState(false);
+  const [txHash, setTxHash] = useState("");
 
   // Mint NFT
   const mint = async () => {
-    setMintin(true);
+    setIsToken(false);
+    setIsFractionate(false);
     fractionalNft.methods
       .mint(22)
       .send({
@@ -20,17 +25,20 @@ const BuyNft = () => {
       .on("transactionHash", (txHash) => {
         setStatus({
           title:'Transaccion Enviada',
-          description: txHash,
+          description: `Por favor espere... 
+                        Hash de la transaccion: ${txHash}`,
           status:'info',
         })
       })
-      .on("receipt", () => {
+      .on("receipt", (tx) => {
+        console.log(true)
+        setTokenId(tx.events.Transfer.returnValues.tokenId);
         setStatus({
           title:'Transaccion confirmada',
           description: 'NFT minted',
           status:'success',
         })
-        setMintin(false);
+        setIsToken(true);
       })
       .on("error", (error) => {
         setStatus({
@@ -38,19 +46,91 @@ const BuyNft = () => {
           description: error.message,
           status:'error',
         })
-        setMintin(false);
       });
+  }
+
+  // Fractionate NFT
+  const fractionate = async () => {
+    fractionalNft.methods
+      .fractionate(tokenId, slices)
+      .send({
+        from: account,
+      })
+      .on("transactionHash", (txHash) => {
+        setStatus({
+          title:'Transaccion Enviada',
+          description: `Por favor espere... 
+                        Hash de la transaccion: ${<a href={`https://mumbai.polygonscan.com/tx/{txHash}`}>{txHash}</a>}`,
+          status:'info',
+        })
+      })
+      .on("receipt", (tx) => {
+        setTxHash(tx.transactionHash);  
+        console.log(tx);
+        setStatus({
+          title:'Transaccion confirmada',
+          description: 'NFT fraccionado en ' + slices + ' partes',
+          status:'success',
+        })
+        setIsFractionate(true);
+      })
+      .on("error", (error) => {
+        setStatus({
+          title:'Transaccion Fallida',
+          description: error.message,
+          status:'error',
+        })
+      });
+  }
+
+  // See NFT for 
+  const buyFraccion = async () => {
+    fractionalNft.methods
+      .buyFraccion(tokenId, slices)
   }
 
   return (
     <div>
-      <h2>Pagina de BuyNft</h2>
-      <button className="mainLayout-header-nav-button1" onClick={() => mint()}>Buy Nft</button>
-      <div>
-        <p>Title: {status.title}</p>
-        <p>Hash de la transaccion: {status.description}</p>
-        <p>status: {status.status}</p>
-      </div>
+      <h2>BuyNft Page</h2>
+        <button className="mainLayout-header-nav-button1" onClick={() => mint()}>Buy Nft</button>
+        <div>
+          <br></br>
+          <p>Title: {status.title}</p>
+          <p>Transaction Hash: {status.description}</p>
+          <p>status: {status.status}</p>
+        </div>
+        <br></br>
+      {
+      isToken?
+        <>
+          <p>Nft Id: {tokenId}</p>
+          <button className="mainLayout-header-nav-button1" onClick={() => fractionate()}>Fractionate</button>
+          <input type={"number"} defaultValue="2" className="input-fractions" onChange={(e) => setSlices(e.target.value)}></input>
+          <br></br>
+        </>
+        :""
+      }
+      {
+      isFractionate?
+        <>
+          <br></br>
+          <p>Nft #{tokenId} fractionated in {slices} parts</p>
+          <p>Transaction Hash: <a href={`https://mumbai.polygonscan.com/tx/${txHash}`}>{txHash}</a></p>
+        </>
+        :""
+      }
+        <>
+          <p>Buy fraccion</p>
+          <button className="mainLayout-header-nav-button1" onClick={() => fractionate()}>Buy</button>
+          <input type={"number"} defaultValue="2" className="input-fractions" onChange={(e) => setSlices(e.target.value)}></input>
+          <br></br>
+        </>
+        <>
+          <p>Sell fraction</p>
+          <button className="mainLayout-header-nav-button1" onClick={() => fractionate()}>Fraccionnar</button>
+          <input type={"number"} defaultValue="2" className="input-fractions" onChange={(e) => setSlices(e.target.value)}></input>
+          <br></br>
+        </>
     </div>
   );
 };
